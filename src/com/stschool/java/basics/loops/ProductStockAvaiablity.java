093f566df164c6bc597c4435db0c0f2d16dd107a
Kasarlaaryan <kasarlaaryan@gmail.com>
2026-01-28 09:43:22 +0530

    feast : loops

--- src/com/stschool/java/basics/loops/ProductStockAvaiablity.java
+++ src/com/stschool/java/basics/loops/ProductStockAvaiablity.java
@@ -9,29 +9,64 @@ public class ProductStockAvaiablity {
         System.out.println("Welcome To My Ecom Portal ");
         System.out.println("Please Enter 1 for Products");
         System.out.println("Please Enter 2 for Exits");
-        char menu;
-        int avaiableStock, originalProductPrice ;
-        menu=sc.next().charAt(0);
+        char menu, continueShopping;
+        float centralTax, stateTax, afterDiscountPrice, centralTaxPercentage, stateTaxPercentage, totalAmount;
+        int avaiableStock, originalProductPrice, shippingCharges;
+        menu = sc.next().charAt(0);
+        centralTaxPercentage = 2.5F;
+        stateTaxPercentage = 2.5F;
+        shippingCharges = 120;
 
-        switch (menu){
+
+        switch (menu) {
             case '1':
                 System.out.println("Please Enter Your Product Price");
                 originalProductPrice = sc.nextInt();
                 avaiableStock = 10;
-                int i = 1;
+
+                while (avaiableStock > 0) {
+                    centralTax = (originalProductPrice * centralTaxPercentage) / 100;
+                    stateTax = (originalProductPrice * stateTaxPercentage) / 100;
+                    totalAmount = originalProductPrice + centralTax + stateTax + shippingCharges;
+                    System.out.println("=====================================================");
+                    System.out.println("CGST Amount on the Product : " + centralTax);
+                    System.out.println("SGST Amount of the Product : " + stateTax);
+                    System.out.println("Shipping Charges : " + shippingCharges);
+                    System.out.println("=====================================================");
+                    System.out.println("Total Price Cost is : " + totalAmount);
+                    System.out.println("=====================================================");
+
+                    System.out.println("Stock Left : " + avaiableStock);
+
+
+                System.out.println("Do you Want to Continue the Shopping ( Y/N ) :  ");
+                continueShopping = sc.next().charAt(0);
+                if (continueShopping == 'Y' || continueShopping == 'y') {
+                    avaiableStock--;
+                } else {
+                    System.out.println("Thank you for shopping with us");
+                    System.exit(0);
+
+                }
+
+                }
+
+                if( avaiableStock == 0){
+                System.out.println("No Stock Avaiable in the store");
+            }
 
 
 
+                break;
 
-
-
-                    break;
             case '2':
                 System.out.println("You Are Exited from the Portal");
                 System.exit(0);
+
+            default:
+                System.out.println("Invalid Input");
+
         }
-
-
 
 
         sc.close();
